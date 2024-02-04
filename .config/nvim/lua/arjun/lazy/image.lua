1915367e24f9d58a3146a9b4aa87ea27b0a1f6ae
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-02-03 16:52:06 -0800

    image.nvim experimentation

---
+++ .config/nvim/lua/arjun/lazy/image.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "3rd/image.nvim",
+        config = function()
+            require("image").setup({
+                  backend = "ueberzug",
+              })
+        end,
+    }
+}
